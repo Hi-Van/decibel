c23f327970a5d84805783c585509ad9c5ea50a88
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2021-05-21 00:49:31 -0400

    prototype highlighting currently active pages and buttons. Changes to section sizings

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import logo from './undraw_podcast_q6p7.svg';
 import qa from './undraw_online_test_gba7.svg';
 import { AiOutlineAudio } from 'react-icons/ai';
+import { FcHome, FcKindle } from 'react-icons/fc';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <nav class='navbar'>
+      <nav className='navbar'>
         <div className='Branding'><AiOutlineAudio />Decibel</div>
         <ul className='navbar-links'>
-          <li>Home</li>
-          <li>Notes</li>
+          <li style={{borderBottom: "2px solid #6C63FF"}}><FcHome/> Home</li>
+          <li style= {{opacity: '60%'}}><FcKindle/> Notes</li>
           <li><button>Login</button></li>
         </ul>
       </nav>
@@ -21,9 +22,9 @@ function App() {
         <p>love the sound of it!</p>
       </header>
       <div className='about'>
-        <div style={{width: '80%'}}>
+        <div className='about-text'>
           <h1>What is Decibel?</h1>
-          <p>Decibel is an audio based note taking app, it's made for nerds by nerds.
+          <p>Decibel is an audio based note taking app, it's made for nerds by nerds!
           Oftentimes its much quicker and more convient to make notes using audio samples when compared to writing notes manually.
           Decibel aims to utilize this to your advantage, we take the notes you would normally write as audio recordings that you provide. We save them, so you can use them at your leisure.</p>
         </div>
